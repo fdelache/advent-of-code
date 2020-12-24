3418dc440a59db98dcd0625e953f422c4d8037d8
Franck Delache <franck.delache@shopify.com>
2020-12-24 00:34:31 -0500

    Day 20 solution - part 2

--- src/year2020/day20.rb
+++ src/year2020/day20.rb
@@ -3,6 +3,11 @@ module Year2020
     class Tile
       attr_reader :id, :content, :north, :south, :east, :west, :sides
 
+      NORTH = 0
+      SOUTH = 1
+      EAST = 2
+      WEST = 3
+
       def self.parse(tile_data)
         lines = tile_data.split("\n")
         /Tile (?<id>\d+):/ =~ lines.shift
@@ -22,17 +27,11 @@ module Year2020
       end
 
       def matching_side_orientation(other_tile)
-        matching_side = sides.find do |side|
+        _matching_side, matching_index = sides.each_with_index.find do |side, _index|
           other_tile.sides.include?(side) || other_tile.sides.include?(side.reverse)
         end
 
-        orientations = ""
-        matching_sides.each do |s|
-          case s
-          when north == s
-          end
-        end
-        sides.find_index
+        matching_index
       end
 
       def rotate
@@ -47,6 +46,20 @@ module Year2020
 
       def flip_h
         self.content = content.reverse
+      end
+
+      def each_transformation
+        Enumerator.new do |y|
+          transformations = [:rotate, :flip_v, :rotate, :flip_h, :rotate, :flip_v, :rotate, :flip_h]
+          transformations.each do |m|
+            self.send(m)
+            y << self
+          end
+        end
+      end
+
+      def inside
+        content[1..-2].map { |s| s[1..-2] }
       end
 
       private
@@ -81,12 +94,88 @@ module Year2020
         end
       end
 
+      def find_adjacent(tile)
+        tiles.reject { |other| other == tile }
+             .select { |other| tile.matching_side?(other) }
+      end
+
       def assemble
         corners = find_corners
 
         map_size = Math.sqrt(tiles.size)
         positions = Array.new(map_size) { Array.new(map_size) }
-        positions[0][0] = corners.first
+        top_left = corners.first
+        adjacent = find_adjacent(top_left)
+        top_left.each_transformation.find do |tile|
+          matching_orientations = adjacent.map { |a| tile.matching_side_orientation(a) }.sort
+          matching_orientations == [Tile::SOUTH, Tile::EAST]
+        end
+
+        positions[0][0] = top_left
+        previous = top_left
+
+        (0...map_size).each do |y|
+          (1...map_size).each do |x|
+            next_tile = find_adjacent(previous).find do |t|
+              t.each_transformation.any? do |m|
+                m.sides[Tile::WEST] == previous.sides[Tile::EAST]
+              end
+            end
+            positions[y][x] = next_tile
+            previous = next_tile
+          end
+
+          previous = positions[y][0]
+          next_tile = find_adjacent(previous).find do |t|
+            t.each_transformation.any? do |m|
+              m.sides[Tile::NORTH] == previous.sides[Tile::SOUTH]
+            end
+          end
+          positions[y+1][0] = next_tile unless y == (map_size-1)
+          previous = next_tile
+        end
+
+        BigMap.new(positions, map_size)
+      end
+    end
+
+    class BigMap < Tile
+      attr_reader :map_size
+
+      def initialize(tile_positions, map_size)
+        content = (0...map_size).map do |y|
+          (0...map_size).map do |x|
+            tile_positions[y][x].inside
+          end.transpose.map(&:join)
+        end.flatten
+        super(0, content)
+        @map_size = content.size
+      end
+
+      def count_sea_monsters
+        (1...(map_size-1)).sum do |y|
+          (0..(map_size-20)).count do |x|
+            sea_monster_at?(x, y)
+          end
+        end
+      end
+
+      def max_sea_monsters
+        each_transformation.map(&:count_sea_monsters).max
+      end
+
+      def sea_monster_at?(x, y)
+        return false unless content[y][x..-1].match?(/^#.{4}##.{4}##.{4}###/)
+        return false unless content[y-1][x+18] == "#"
+        content[y+1][x..-1].match?(/^.#..#..#..#..#..#/)
+      end
+
+      def water_roughness
+        count = @content.sum do |row|
+          row.each_char.count { |c| c == "#" }
+        end
+
+        count - max_sea_monsters * 15
       end
     end
 
@@ -99,7 +188,8 @@ module Year2020
 
     def part2(input)
       map = Map.parse(input)
-      corners = map.find_corners
+      big_map = map.assemble
+      big_map.water_roughness
     end
   end
 end
