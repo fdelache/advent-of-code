ae9e5f3607b4d263cd119cd7b54a1ea90c92c934
Franck Delache <franck.delache@shopify.com>
2021-12-09 23:00:06 -0500

    Update spec template

--- lib/puzzle_spec.rb
+++ lib/puzzle_spec.rb
@@ -16,14 +16,18 @@ class PuzzleSpec
       require 'spec_helper'
 
       RSpec.describe Year#{year}::Day#{day} do
+        let(:sample) {<<-EOF
+        EOF
+        }
+
         it "solves part1" do
           d = Year#{year}::Day#{day}.new
-          expect(d.part1('some_input')).to eq('expected_result')
+          expect(d.part1(sample)).to eq('expected_result')
         end
 
         it "solves part2" do
           d = Year#{year}::Day#{day}.new
-          expect(d.part2('some_input')).to eq(nil)
+          expect(d.part2(sample)).to eq(nil)
         end
       end
     TPL
